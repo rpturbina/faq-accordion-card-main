268950fa8b9d5319b2eb73cccc7cbc1e3cdae235
rpturbina <rizki.pratama.turbina@gmail.com>
2022-03-08 04:19:41 +0700

    Add accordion user interactive function

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const accordionButtons = document.querySelectorAll(".accordion__button");
+console.log(accordionButtons);
+
+accordionButtons.forEach((accordionButton) => {
+  accordionButton.addEventListener("click", (event) => {
+    console.log(event.target);
+    const target = event.target;
+    const accordionBody = target.parentElement.nextElementSibling;
+
+    target.classList.toggle("accordion__button--collapsed")
+    accordionBody.classList.toggle("accordion__body--show");
+  });
+});
